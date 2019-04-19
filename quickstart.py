8c6d141b55986387f026098de003781e17e31e86
Yogita Chouhan <yogita.chouhan@northout.com>
2019-04-19 15:26:34 +0530

    inital commit

---
+++ quickstart.py
@@ -0,0 +1,26 @@
+from httplib2 import Http
+from json import dumps
+
+#
+# Hangouts Chat incoming webhook quickstart
+#
+def main():
+    url = 'https://chat.googleapis.com/v1/spaces/AAAAV3sXpwU/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=BxzvzKx5_yU6Zb4DufFRZDXbH6iVPg7p7BCo-KDy2e0%3D'
+    bot_message = {
+        'text' : 'Hello from Python script!'}
+
+    message_headers = { 'Content-Type': 'application/json; charset=UTF-8'}
+
+    http_obj = Http()
+
+    response = http_obj.request(
+        uri=url,
+        method='POST',
+        headers=message_headers,
+        body=dumps(bot_message),
+    )
+
+    print(response)
+
+if __name__ == '__main__':
+    main()
